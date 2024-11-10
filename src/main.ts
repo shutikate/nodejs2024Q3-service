a9fabd63841297fbd5b3f2f10b7d59fa2cf0c1ff
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2024-11-10 23:40:43 +0100

    fix: api.yaml file, add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as YAML from 'yamljs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +16,9 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
+
+  const document: OpenAPIObject = YAML.load('doc/api.yaml');
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(port);
 }
